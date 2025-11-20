9835aa944399d35526e92e14dbee939504d0be83
helmutqualtinger <qualcunodue@gmail.com>
2025-11-20 15:33:42 +0100

    Füge Muster-Modal und Musterdefinitionen für das Spiel "Game of Life" hinzu

--- patterns.py
+++ patterns.py
@@ -1,13 +1,19 @@
 PATTERNS = {
+    # Oscillators
     "blinker": """
 ..O..
 ..O..
 ..O..
 """,
-    "glider": """
-.O.
-..O
-OOO
+    "toad": """
+.OOO
+OOO.
+""",
+    "beacon": """
+OO..
+OO..
+..OO
+..OO
 """,
     "pulsar": """
 ...OOO...OOO...
@@ -23,5 +29,124 @@ OOO
 .O....O.O....O.
 .................
 ...OOO...OOO...
+""",
+    "penta_decathlon": """
+..OOO..
+O.....O
+O.....O
+O.....O
+..OOO..
+""",
+
+    # Spaceships
+    "glider": """
+.O.
+..O
+OOO
+""",
+    "lwss": """
+.O..O
+O.....
+O.O..O
+.OOOO.
+""",
+    "mwss": """
+.O...O
+O......
+O..O..O
+.OOOOO.
+""",
+    "hwss": """
+.O....O
+O.......
+O...O..O
+.OOOOOO.
+""",
+
+    # Still lifes
+    "block": """
+OO
+OO
+""",
+    "tub": """
+.O.
+OOO
+.O.
+""",
+    "beehive": """
+.OO.
+O..O
+.OO.
+""",
+    "loaf": """
+.OO.
+O..O
+.O.O
+..O.
+""",
+    "boat": """
+O.O
+OOO
+.O.
+""",
+    "pond": """
+.OO..
+O..O.
+O..O.
+.OO..
+""",
+    "ship": """
+O.O.
+OOO.
+.O..
+""",
+    "aircraft": """
+..O.
+O...
+OOO.
+.O.O
+""",
+
+    # Guns & Puffers
+    "gosper_glider_gun": """
+........................O...........
+......................O.O...........
+............OO......OO............OO
+...........O...O....OO............OO
+OO........O.....O...OO..............
+OO........O...O.OO....O.O...........
+..........O.....O.......O...........
+...........O...O....................
+............OO.......................
+""",
+
+    # Other patterns
+    "beehive_with_tail": """
+.OO.
+O..O
+.OO.
+.O..
+""",
+    "pi_heptomino": """
+OOO
+O.O
+.OO
+""",
+    "b_heptomino": """
+.OO
+OO.
+.O.
+.O.
+""",
+    "acorn": """
+.O....
+...O..
+OO.OOO
+""",
+    "die_hard": """
+......O
+OO.....
+.O...OO
+...O...
 """,
 }
